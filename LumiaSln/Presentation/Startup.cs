84ec3fba9ef7fa069e9f9859f3e5121f58c2fd64
Jeann Andrade <jeann.andrade@outlook.com>
2020-06-22 21:54:02 -0300

    Preparing the project structure

--- LumiaSln/Presentation/Startup.cs
+++ LumiaSln/Presentation/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Lumia
+namespace Presentation
 {
     public class Startup
     {
