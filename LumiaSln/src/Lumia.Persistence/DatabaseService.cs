8a6ca3b8b48d09f6b202af95e53b2c73f427f165
Jeann Andrade <jeann.andrade@outlook.com>
2020-08-02 22:24:56 -0300

    Camada de persistência

---
+++ LumiaSln/src/Lumia.Persistence/DatabaseService.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Lumia.Application.Interfaces;
+using Lumia.Domain.Clientes;
+using Lumia.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using Lumia.Persistence.Mappings;
+
+namespace Lumia.Persistence
+{
+    public class DatabaseService : DbContext
+    {
+        private readonly string connectionString;
+
+        public DatabaseService(string connectionString)
+        {
+            if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("Value should not be empty.", nameof(connectionString));
+
+            this.connectionString = connectionString;
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.AddConfiguration(new ClienteMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(this.connectionString);
+        }
+    }
+}
---
+++ LumiaSln/src/Lumia.Persistence/Mappings/ClienteMapping.cs
@@ -0,0 +1,49 @@
+﻿using Lumia.Domain.Clientes;
+using Lumia.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lumia.Persistence.Mappings
+{
+    public class ClienteMapping : EntityTypeConfiguration<Cliente>
+    {
+        public override void Map(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.Property(e => e.Nome)
+               .HasColumnType("varchar(150)")
+               .IsRequired();
+
+            builder.Property(e => e.Email)
+                .HasColumnType("varchar(150)");
+
+            builder.Property(e => e.Endereco)
+                .HasColumnType("varchar(250)");
+
+            builder.Property(e => e.Telefone)
+                .HasColumnType("varchar(15)");
+
+            builder.Property(e => e.Observacao)
+                .HasColumnType("varchar(1000)");
+
+            //builder.Ignore(e => e.ValidationResult);
+
+            //builder.Ignore(e => e.Tags);
+
+            //builder.Ignore(e => e.CascadeMode);
+
+            //builder.ToTable("Eventos");
+
+            //builder.HasOne(e => e.Organizador)
+            //    .WithMany(o => o.Eventos)
+            //    .HasForeignKey(e => e.OrganizadorId);
+
+            //builder.HasOne(e => e.Categoria)
+            //    .WithMany(e => e.Eventos)
+            //    .HasForeignKey(e => e.CategoriaId)
+            //    .IsRequired(false);
+        }
+    }
+}
